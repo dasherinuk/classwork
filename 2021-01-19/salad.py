9ad95b0d451b9907d54a54b404d295b38832d448
dasherinuk <dasherpegusus@gmail.com>
2021-01-30 11:51:09 +0000

    30/1/21

--- 2021-01-19/salad.py
+++ 2021-01-19/salad.py
@@ -3,6 +3,7 @@ class Salad:
         self.__name=name
         self.__lettuce=None
         self.__carrot=None
+        self.__mass=None
 
     def add_carrot(self,carrot):
         if type (carrot)==Carrot:
@@ -11,6 +12,12 @@ class Salad:
     def add_lettuce(self,lettuce):
         if type (lettuce)==Lettuce:
             self.__lettuce=lettuce
+
+    
+        
+
+    def __str__(self):
+        return f"Salad: name={self.__name} "
 
 
 class Lettuce:
@@ -21,6 +28,9 @@ class Lettuce:
 
     def __str__(self):
         return "Lettuce: "+ self.__name
+
+    def __str__(self):
+        return f"Lettuce: name={self.__name} "
 
     #def mass_lettuce(self):
         #mass_lettuce = 1
@@ -35,16 +45,22 @@ class Carrot:
 
     def __str__(self):
         return "Carrot: "+ self.__name
+    def __str__(self):
+        return f"Carrot: name={self.__name} "
+
 
     #def mass_carrot(self):
         #mass_carrot = 1
-
-salad.add_carrot(carrot)
-salad.add_lettuce(lettuce)
+salad=Salad("carrot and lettuce")
+carrot=Carrot("carrot")
+lettuce=Lettuce("lettuce")
+salad.add_lettuce
+salad.add_carrot
 print(salad)
+carrot.self.__mass
+#mass=carrot.self.__mass + lettuce.self.__mass
 
 
 
 
-salad = Salad("Salad")
-salad.
+
